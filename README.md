1271918fb506ad91da3daba5c2d3804bd30b1203
safa <1439325342@qq.com>
2023-08-03 11:42:23 +0800

    update 0.1.0

--- README.md
+++ README.md
@@ -1,34 +1,53 @@
 # join-files
 join File|Blob|string|Object into a single file, and restore it at any time
-
 ```typescript
-
 import { joinFiles , splitFiles , FileItem } from 'join-files'
 
-const input = document.createElement('input')
-input.type = 'file'
-input.multiple = true
-input.onchange = async ()=>{
-    const files: FileItem[] = Array.from(input.files)
+const files: FileItem[] = []
 
-    files.push({
-        name : 'some text',
-        data : "123123",
-    })
-    files.push({
-        name : 'some object data',
-        data : {
-            name : 'safa',
-            age : 9999
-        },
-    })
-    console.log( files )
+files.push({
+    name : 'some text',
+    data : "123123",
+})
+files.push({
+    name : 'some object data',
+    data : {
+        name : 'safa',
+        age : 9999
+    },
+})
+files.push(
+    new File(['some string'] , 'some.txt')
+)
+console.log( files )
 
-    const result = await joinFiles(files)
-    console.log( result )
+const result = await joinFiles(files)
+console.log( result )
 
-    const _files = await splitFiles(result)
-    console.log( _files )
-}
-input.click()
+const _files = await splitFiles(result)
+console.log( _files )
+```
+
+convert a File to imageFile , and restore it at any time
+```typescript
+import { fileToImageFile , imageFileToFile } from 'join-files'
+
+const file = new File(['some string'] , 'some.txt')
+console.log(file)
+
+const imageFile = await fileToImageFile(file)
+console.log(imageFile)
+
+const _file = await imageFileToFile(imageFile)
+console.log(_file)
+
+```
+
+in js
+```html
+<script type='module'>
+    import { joinFiles , splitFiles } from 'https://www.unpkg.com/join-files@0.10.0/index.js'
+    // ...
+</script>
+
 ```
--- index.ts
+++ index.ts
@@ -1,18 +1,159 @@
-interface FileMetaData{
-    name : string
-    size : number
-    mimeType ?: string,
-    type : 'file'|'blob'|'string'|'object'
+export interface JoinedFileMetaData{
+    files : FileMetaData[]
+    md5 : string
+    version : string
 }
 
-export type FileItem = File | {name:string , data:Blob|string|Object }
+export interface FileMetaData{
+    parse():Promise<FileItem>
+    parse(targetType:'string'):Promise<string>
+    parse<T>(targetType:'object'):Promise<T>
+    parse(targetType:'file'):Promise<File>
+    parse(targetType:'blob'):Promise<Blob>
+}
+
+let canvas = null as unknown as HTMLCanvasElement
+let ctx = null as unknown as CanvasRenderingContext2D
+
+
+export class FileMetaData{
+    name !: string
+    size !: number
+    start ?: number
+    end ?: number
+    mimeType ?: string
+    joinedFile !: Blob
+    JoinedFileMetaData !: JoinedFileMetaData
+
+    type !: 'file'|'blob'|'string'|'object'
+
+    constructor( meta ?: Partial<FileMetaData> ){
+        if( meta ){
+            Object.assign(this, meta)
+        }
+    }
+
+    private async parseNative(data:Blob) : Promise<FileItem> {
+        const { name , type } = this
+
+        if( type == 'file' ){
+            return new File( [data] , name , { type : this.mimeType } )
+        }
+
+        if( type == 'blob' ){
+            return { name , data }
+        }
+
+        const stringData = this.JoinedFileMetaData.version == '' ? uint8ArrayToString( new Uint8Array( await data.arrayBuffer() ) ) : await data.text()
+
+        if( type == 'object'){
+            return { name, data : JSON.parse(stringData) }
+        }
+        return { name : name, data : stringData }    
+    }
+
+    private getBlobData(defaultData:FileItem){
+        return defaultData instanceof File ? defaultData : (defaultData.data as Blob)
+    }
+    private getStringData(defaultData:FileItem){
+        return (defaultData as {data:string} ).data
+    }
+    private getObjectData(defaultData:FileItem){
+        return (defaultData as {data:Object} ).data
+    }
+
+    async parse<T extends any>( targetType ?: 'string'|'object'|'blob'|'file' ) : Promise<FileItem|File|Blob|string|T> {
+        const { start , end , name , type , mimeType  } = this
+
+        const data = this.joinedFile.slice(start,end , mimeType || '')
+        const result = await this.parseNative(data)
+
+        if( !targetType ){
+            return result
+        }
+
+        if( targetType == 'string' ){
+            if( type == 'string' ){
+                return this.getStringData(result)
+            }
+            if( type == 'object' ){
+                return JSON.stringify( this.getObjectData(result) )
+            }
+
+            return this.getBlobData(result).text()
+        }
+
+        if( targetType == 'object' ){
+            if( type == 'string' ){
+                return JSON.parse( this.getStringData(result) ) as T
+            }
+            if( type == 'object' ){
+                return this.getObjectData(result) as T
+            }
+
+            return JSON.parse( await this.getBlobData(result).text() ) as T
+        }
+
+        if( targetType == 'blob' ){
+            return data
+        }
+
+        if( targetType == 'file' ){
+            if( type == 'file' ){
+                return result
+            }
+            if( type == 'blob' ){
+                return new File([data] , name)
+            }
+        }
+
+        return null as unknown as T
+    }
+}
+
+export type FileItem = File | {
+    name:string , 
+    data:Blob|string|Object 
+}
 
 export async function parseMataData(joinedFile:Blob){
     try {
-        const mateDataLength = new Uint32Array( await joinedFile.slice(0,4).arrayBuffer() )
-        const metaData = new Uint8Array( await joinedFile.slice(4,4 + mateDataLength[0]).arrayBuffer() )    
-        const metaJson = JSON.parse( uint8ArrayToString( metaData ) ) as FileMetaData[]
-        return metaJson
+
+        const joinedFileMetaLength = new Uint32Array( await joinedFile.slice(0,4).arrayBuffer() )[0]
+        const joinedFileMetaBlob = joinedFile.slice(4,4 + joinedFileMetaLength)
+        let joinedFileMetaStr = await joinedFileMetaBlob.text()
+
+        if( joinedFileMetaStr[0] == '[' ){
+            joinedFileMetaStr = uint8ArrayToString( new Uint8Array( await joinedFileMetaBlob.arrayBuffer() ) )
+        }
+
+        const joinedFileMetaData : JoinedFileMetaData = {
+            files : [],
+            version : '',
+            md5 : '',
+        }
+
+        const joinedFileMetaDataResult = JSON.parse( joinedFileMetaStr )
+
+        if( Array.isArray( joinedFileMetaDataResult ) ){
+            joinedFileMetaData.files = joinedFileMetaDataResult
+        }
+        else{
+            Object.assign(joinedFileMetaData, joinedFileMetaDataResult)
+        }
+
+        let start = joinedFileMetaLength + 4
+        joinedFileMetaData.files = joinedFileMetaData.files.map((meta)=>{
+            meta.start = start
+            meta.end = start + meta.size
+            meta.joinedFile = joinedFile
+            meta.JoinedFileMetaData = joinedFileMetaData
+
+            start = meta.end
+            return new FileMetaData(meta)
+        })
+
+        return joinedFileMetaData
     }
     catch (error) {
         console.error("parse fail !")
@@ -20,44 +161,20 @@ export async function parseMataData(joinedFile:Blob){
     }
 }
 
-export async function splitFiles( joinedFile:Blob , filename:string|string[] = '' ) : Promise<FileItem[]> {
+export async function splitFiles( joinedFile:Blob , filename:string|string[] = '') : Promise<FileItem[]> {
     let targetFilenames : string[]
 
     if( filename ){
         targetFilenames = Array.isArray(filename) ? filename : [filename]
     }
     try {
-        const mateDataLength = new Uint32Array( await joinedFile.slice(0,4).arrayBuffer() )
-        const metaJson = await parseMataData(joinedFile)
-        let dataStart = 4 + mateDataLength[0]
-
+        const joinedFileMetaData = await parseMataData(joinedFile)
         const files = await Promise.all(
-            metaJson.map(async ({ name , size , type , mimeType })=>{
-                const start = dataStart
-                const end = dataStart + size
-                dataStart = end
-
-                if( targetFilenames?.indexOf(name) == -1 ){
+            joinedFileMetaData.files.map(async (meta)=>{
+                if( targetFilenames?.indexOf(meta.name) == -1 ){
                     return null
                 }
-
-                const data = joinedFile.slice(start,end)
-
-                if( type == 'file' ){
-                    return new File( [data] , name , mimeType ? { type : mimeType } : {} )
-                }
-
-                if( type == 'blob' ){
-                    return { name , data }
-                }
-
-                const stringData = uint8ArrayToString( new Uint8Array(await data.arrayBuffer() ) )
-
-                if( type == 'object'){
-                    return { name, data : JSON.parse(stringData) }
-                }
-
-                return { name : name, data : stringData }            
+                return meta.parse()
             })
         )
         return files.filter(fileItem=>fileItem) as FileItem[]
@@ -69,46 +186,160 @@ export async function splitFiles( joinedFile:Blob , filename:string|string[] = '
 }
 
 export async function joinFiles(files: FileItem[] , filename = Date.now() + '.data' ){
-    const datas = [] as Uint8Array[]
-    const metaJson = [] as FileMetaData[]
+    const blobs = [] as Blob[]
+    const _files = [] as FileMetaData[]
 
     for (const item of files) {
 
-        const fileMetaData : FileMetaData = {
+        const fileMetaData = new FileMetaData({
             name : item.name,
             type : 'file',
             size : 0,
-        }
+        })
 
-        let file_uint8 : Uint8Array
+        let blobData : Blob
 
         if( item instanceof File){
             fileMetaData.type = 'file'
             fileMetaData.mimeType = item.type
-            file_uint8 = new Uint8Array( await item.arrayBuffer() )
+            blobData = item
         }
         else if( item.data instanceof Blob){
             fileMetaData.type = 'blob'
-            file_uint8 = new Uint8Array( await item.data.arrayBuffer() )
+            fileMetaData.mimeType = item.data.type
+            blobData = item.data
         }
         else if( typeof item.data == 'string' ){
             fileMetaData.type = 'string'
-            file_uint8 = stringToUint8Array(item.data)
+            blobData = new Blob([ item.data ])
         }
         else{
             fileMetaData.type = 'object'
-            file_uint8 = stringToUint8Array(JSON.stringify(item.data))
+            blobData = new Blob([ JSON.stringify(item.data) ])
         }
 
-        fileMetaData.size = file_uint8.length
-        datas.push(file_uint8)
-        metaJson.push(fileMetaData)
+        fileMetaData.size = blobData.size
+        blobs.push(blobData)
+        _files.push(fileMetaData)
     }
 
-    const metaData = stringToUint8Array(JSON.stringify(metaJson))
-    const metaDataLength = new Uint8Array(new Uint32Array([ metaData.length ]).buffer )
+    const joinedFileMetaData = {
+        files: _files,
+        md5 : '',
+        version : '0.1.0'
+    }
 
-    return new File([ metaDataLength , metaData , ...datas ] , filename , { type : 'application/octet-stream' })
+    const joinedFileMetaBlob = new Blob([ JSON.stringify(joinedFileMetaData) ])
+    const joinedFileMetaLength = new Uint8Array( new Uint32Array([ joinedFileMetaBlob.size ]).buffer )
+
+    return new File([ 
+        joinedFileMetaLength , 
+        joinedFileMetaBlob , 
+        ...blobs 
+    ] , filename , { type : 'application/octet-stream' })
+}
+
+
+
+export async function imageFileToFile(url:string|Blob) {
+    const image = await toImageSource(url)
+
+    initCanvas(image.width, image.height)
+    ctx.drawImage(image, 0, 0)
+
+    const data_rgba = ctx.getImageData(0, 0, image.width, image.height).data
+    const data_all = [] as number[]
+    for (let i = 0; i < data_rgba.length; i+=4 ) {
+        data_all.push(data_rgba[i] , data_rgba[i+1] , data_rgba[i+2])
+    }
+
+    const data_all_u8 = new Uint8Array(data_all)
+    const meta_u32 = new Uint32Array( data_all_u8.slice(0,8).buffer )
+    const data_size = meta_u32[1]
+    const filename_size = meta_u32[0]
+
+    const filename = await new Blob( [data_all_u8.slice(8,8+filename_size)] ).text()
+    const file = new File([ data_all_u8.slice(8+filename_size , 8+filename_size+data_size) ],filename)
+
+    canvas.width = 0
+    canvas.height = 0
+
+    return file
+}
+
+
+export async function fileToImageFile(file:File|Blob , filename = '') {
+
+    // imageSize 6400 x 6400
+    if( file.size > 1024 * 200 * 200 ){
+        throw new Error("file too big !");
+    }
+
+    const _filename = filename || (file instanceof File ? file.name : '')
+    const file_u8 = new Uint8Array( await file.arrayBuffer() )
+    const file_size = file_u8.length
+    const filename_u8 = new Uint8Array( await new Blob([_filename] ).arrayBuffer() )
+    const filename_size = filename_u8.length
+
+    const meta_u8 = new Uint8Array(new Uint32Array([ filename_size , file_size ]).buffer)
+    const data_all = [ ...meta_u8, ...filename_u8  , ...file_u8 ]
+    const data_rgba = [] as number[]
+
+    for (let i = 0; i < data_all.length; i+=3) {
+        data_rgba.push( data_all[i] , data_all[i+1]||0 , data_all[i+2]||0 , 255 )
+    }
+
+    const imageSize = Math.floor( Math.sqrt(file_u8.length / 3) ) + 1
+    const imageData = new ImageData(imageSize , imageSize)
+    imageData.data.set(data_rgba)
+
+    initCanvas(imageSize , imageSize)
+    ctx.putImageData(imageData, 0, 0 )
+
+    return await new Promise<File>((reject)=>{
+        canvas.toBlob((blob)=>{
+            const extname = blob!.type == 'image/webp' ? '.webp' : '.png'
+            const file = new File([ blob! ] , _filename + extname)
+            reject(file)
+        },'image/webp',1)
+    })
+}
+
+
+function initCanvas(width=0,height=0) {
+    if( canvas == null ){
+        canvas = document.createElement('canvas')
+        ctx = canvas.getContext('2d')!
+    }
+
+    canvas.width = width || canvas.width
+    canvas.height = height || canvas.height
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+async function toImageSource(url:string|Blob){
+    let _url = url as string
+
+    if( url instanceof Blob ){
+        if( 'createImageBitmap' in window){
+            return createImageBitmap(url)
+        }
+        _url = URL.createObjectURL(url)
+    }
+
+    return await new Promise<HTMLImageElement>((reject,resolve)=>{
+        const _image = new Image()
+        _image.referrerPolicy='no-referrer'
+        _image.crossOrigin = ''
+        _image.src = _url
+        _image.onload = ()=>{
+            reject(_image)
+            if( url != _url ){
+                URL.revokeObjectURL(_url)
+            }
+        }
+        _image.onerror = resolve
+    })
 }
 
 function uint8ArrayToString(data:Uint8Array) {
@@ -120,11 +351,45 @@ function uint8ArrayToString(data:Uint8Array) {
     return str
 }
 
-function stringToUint8Array(str:string){
-    const data_u32 = new Uint32Array(str.length)    
-    for (let i = 0; i < str.length; i++) {
-        data_u32[i] = str.charCodeAt(i)
-    }
-    return new Uint8Array(data_u32.buffer)
-}
+
+// test()
+// async function test() {
+//     const str = 'asdas asd asd asde 2 ad asd asd asd啥地方'
+//     const testFile = new File([ str ],'')
+//     const imageFile = await fileToImageFile(testFile)
+//     const decodedResult = await imageFileToFile(imageFile)
+//     console.log( str == await decodedResult.text() ,  imageFile , testFile , decodedResult )
+// }
+
+
+// test_2()
+// async function test_2() {
+//     const str = 'asdas asd asd asde 2 ad asd asd asd啥地方'
+//     const obj = {
+//         aaa : 12323,
+//         bbb : str
+//     }
+//     const file = new File(['console.log("hello")'],'some.js',{ type : 'text/javascript' })
+
+//     const testInput = [
+//         { name : 'str' , data : str },
+//         { name : 'obj' , data : obj },
+//         file,
+//     ]
+//     console.log( 'testInput' , testInput )
+
+//     const testFile = await joinFiles(testInput , 'test.data')
+//     console.log( 'testFile' , testFile )
+
+//     const imageFile = await fileToImageFile(testFile)
+//     console.log( 'imageFile' , imageFile )
+
+//     const decodedResult = await imageFileToFile(imageFile)
+//     console.log( 'decodedResult' , decodedResult )
+
+//     const splitResult = await splitFiles(decodedResult)
+//     console.log( 'splitResult' , splitResult )
+
+//     console.log( await parseMataData(decodedResult) )
+// }
 
